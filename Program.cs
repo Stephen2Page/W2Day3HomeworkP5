9bbdc9eac69e930a15d6714dd85747cb1dd16b13
Stephen2Page <stephen2page@gmail.com>
2016-09-14 19:56:07 -0400

    final version

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W2Day3HomeworkP5
+{
+    class Program
+    {
+        //Create a method named "Double" to calculate and return an integer number doubled. 
+        //For example. Double(7) should return 14. 
+      
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter and interger");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(Double(number));
+        }
+        static int Double(int number)
+        {
+            int result = number * 2;
+            return result;
+        }
+    }
+}
